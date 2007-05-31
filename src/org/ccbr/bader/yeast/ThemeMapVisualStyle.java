f5fd7839121cee76ae54cca3f3e613ab3f3cda4a
Mike Matan <mmatan>
2007-05-31 23:02:23 +0000

    added new internal class which extends EdgeAppearanceCalculator, for determining the appearance of edges (currently does nothing other than the default)
    Modified node appearance calculator so that node size is determined to be proportional to the number of input network nodes 
    which belong to the theme node (as determined by the attribute recording theme nodes).
    
    Created new method for initializing sub calculators, for use in the constructors.

--- src/org/ccbr/bader/yeast/ThemeMapVisualStyle.java
+++ src/org/ccbr/bader/yeast/ThemeMapVisualStyle.java
@@ -4,12 +4,15 @@ import java.awt.Color;
 import java.awt.Shape;
 import java.util.List;
 
+import giny.model.Edge;
 import giny.model.Node;
 import cytoscape.CyNetwork;
 import cytoscape.Cytoscape;
 import cytoscape.data.CyAttributes;
+import cytoscape.visual.EdgeAppearance;
 import cytoscape.visual.EdgeAppearanceCalculator;
 import cytoscape.visual.GlobalAppearanceCalculator;
+import cytoscape.visual.LineType;
 import cytoscape.visual.NodeAppearance;
 import cytoscape.visual.NodeAppearanceCalculator;
 import cytoscape.visual.ShapeNodeRealizer;
@@ -19,29 +22,59 @@ public class ThemeMapVisualStyle extends VisualStyle {
 
 	public ThemeMapVisualStyle(String name) {
 		super(name);
-		this.setNodeAppearanceCalculator(new ThemeMapNodeAppearanceCalculator());
+		this.setCalculators();
 		// TODO Auto-generated constructor stub
 	}
 
 	public ThemeMapVisualStyle(VisualStyle toCopy) {
 		super(toCopy);
-		this.setNodeAppearanceCalculator(new ThemeMapNodeAppearanceCalculator());
+		this.setCalculators();
 		// TODO Auto-generated constructor stub
 	}
 
 	public ThemeMapVisualStyle(VisualStyle toCopy, String newName) {
 		super(toCopy, newName);
-		this.setNodeAppearanceCalculator(new ThemeMapNodeAppearanceCalculator());
+		this.setCalculators();
 		// TODO Auto-generated constructor stub
 	}
 
 	public ThemeMapVisualStyle(String name, NodeAppearanceCalculator nac,
 			EdgeAppearanceCalculator eac, GlobalAppearanceCalculator gac) {
 		super(name, nac, eac, gac);
-		this.setNodeAppearanceCalculator(new ThemeMapNodeAppearanceCalculator());
+		this.setCalculators();
 		// TODO Auto-generated constructor stub
 	}
 
+	private void setCalculators() {
+		this.setNodeAppearanceCalculator(new ThemeMapNodeAppearanceCalculator());
+		this.setEdgeAppearanceCalculator(new ThemeMapEdgeAppearanceCalculator());
+	}
+	
+	public class ThemeMapEdgeAppearanceCalculator extends EdgeAppearanceCalculator {
+
+		@Override
+		public EdgeAppearance calculateEdgeAppearance(Edge edge, CyNetwork network) {
+			// TODO Auto-generated method stub
+			EdgeAppearance appr = super.calculateEdgeAppearance(edge, network);
+			this.modifyEdgeAppearance(appr, edge, network);
+			return appr;
+		}
+
+		@Override
+		public void calculateEdgeAppearance(EdgeAppearance appr, Edge edge, CyNetwork network) {
+			// TODO Auto-generated method stub
+			super.calculateEdgeAppearance(appr, edge, network);
+		}
+		
+		private void modifyEdgeAppearance(EdgeAppearance appr, Edge edge, CyNetwork network) {
+			this.modifyEdgeAppearance(appr, edge, network);
+			//appr.setLineType(LineType.LINE_3);
+			appr.applyBypass(edge);
+			return;
+		}
+		
+	}
+	
 	public class ThemeMapNodeAppearanceCalculator extends NodeAppearanceCalculator {
 
 		CyAttributes nodeAtt = Cytoscape.getNodeAttributes();
@@ -82,19 +115,19 @@ public class ThemeMapVisualStyle extends VisualStyle {
 		
 		private void modifyNodeAppearance(NodeAppearance appr, Node node, CyNetwork network) {
 			
-//			final int numDirectlyCoveredGenes = GOSlimmerUtil.getNumGenesCoveredByGoNode(node, GOSlimmerGUIViewSettings.includeDescendantInferredCoveredGenesInNodeSizeCalculations,GOSlimmerGUIViewSettings.sizeNodesBasedOnUserGeneAnnotation);
+			final int numDirectlyCoveredGenes = TMUtil.getNumThemeMembers(node);
 			
-//			double nodeDim = numDirectlyCoveredGenes>0?numDirectlyCoveredGenes*minNodeSize:minNodeSize;
-//			nodeDim +=1; //to ensure we don't get any negative values when we calculate the logarithm
-//			if (nodeDim >1) nodeDim = Math.log(nodeDim);
-//			nodeDim *= 10;
-//			
-//			appr.setLabel(node.getIdentifier());
-//			appr.setHeight(nodeDim);
-//			appr.setWidth(nodeDim);
-//			appr.setShape(ShapeNodeRealizer.ELLIPSE);
-//			
-//			
+			double nodeDim = numDirectlyCoveredGenes>0?numDirectlyCoveredGenes*minNodeSize:minNodeSize;
+			nodeDim +=1; //to ensure we don't get any negative values when we calculate the logarithm
+			if (nodeDim >1) nodeDim = Math.log(nodeDim);
+			nodeDim *= 10;
+			
+			appr.setLabel(node.getIdentifier());
+			appr.setHeight(nodeDim);
+			appr.setWidth(nodeDim);
+			appr.setShape(ShapeNodeRealizer.ELLIPSE);
+			
+			
 //			if (isSelectedForSlimSet(node)) { 
 //				appr.setFillColor(selectedNodeColor);
 //			}
@@ -114,7 +147,8 @@ public class ThemeMapVisualStyle extends VisualStyle {
 //			else {
 //				appr.setLabel(node.getIdentifier());
 //			}
-
+			appr.setLabel(node.getIdentifier());
+			
 		}
 		
 	}
