ed14d28b0ee2591f6654b6b6963974cc7b8a4150
Mike Matan <mmatan>
2007-06-10 16:02:17 +0000

    refactored code so that view is created by a separate method from the one which creates the thematic map graph.
    Attribute circle layout is applied to newly created thematic map views
    
    Only a single edge is created between theme nodes, rather than one for each direction in the input edges which connect theme members.
    
    Beginning implementation of hypergeometric test functionality, though it is mostly prototype code at this point.

--- src/org/ccbr/bader/yeast/ThematicMapFunctionPrototype.java
+++ src/org/ccbr/bader/yeast/ThematicMapFunctionPrototype.java
@@ -10,6 +10,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.ccbr.bader.yeast.hypergeometric.DistributionCount;
+import org.ccbr.bader.yeast.hypergeometric.HypergeometricTestCalculate;
+
 import csplugins.layout.algorithms.hierarchicalLayout.HierarchicalLayoutListener;
 import csplugins.layout.algorithms.graphPartition.AttributeCircleLayout;
 import cytoscape.CyNetwork;
@@ -18,6 +21,10 @@ import cytoscape.data.CyAttributes;
 import cytoscape.data.Semantics;
 import cytoscape.view.CyNetworkView;
 import cytoscape.visual.VisualMappingManager;
+import cytoscape.visual.VisualStyle;
+import cytoscape.visual.mappings.ContinuousMapping;
+import cytoscape.visual.mappings.Interpolator;
+import cytoscape.visual.mappings.ObjectMapping;
 
 public class ThematicMapFunctionPrototype {
 
@@ -25,14 +32,19 @@ public class ThematicMapFunctionPrototype {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public static CyNetwork createThematicMap(String themeAttributeName) {
+		CyNetwork inputNetwork = Cytoscape.getCurrentNetwork();
+		return createThematicMap(inputNetwork, themeAttributeName);
+	}
+	
 	/**Creates a new CyNetwork based on the current CyNetwork, using the values of the given themeAttributeName parameter for the nodes 
 	 * of the graph as the themes (to which the nodes belong in the theme graph)
 	 * 
 	 * @param themeAttributeName the attribute on which the thematic map is to be created
 	 */
-	public static void createThematicMap(String themeAttributeName) {
-		CyNetwork inputNetwork = Cytoscape.getCurrentNetwork();
-		CyNetwork thematicMap = Cytoscape.createNetwork(inputNetwork.getTitle() + "_thematic_map");
+	public static CyNetwork createThematicMap(CyNetwork inputNetwork,String themeAttributeName) {
+		
+		CyNetwork thematicMap = Cytoscape.createNetwork(inputNetwork.getTitle() + " - " + themeAttributeName + " thematic map");
 		/*  pseudo code:
 		 *  create a new  attribute on the thematic map group with the name 'node count'
 		 *  (alternatively, create a new list attribute with the name 'input nodes')
@@ -104,25 +116,18 @@ public class ThematicMapFunctionPrototype {
 			connectThemeNodesWithConnectedInputNodes(thema,inputNodeIDToThemes,iedge);
 		}
 		
-		//finally, generate the graph view
-		CyNetworkView themaView = Cytoscape.createNetworkView(thema);
-		
-		VisualMappingManager vmm = Cytoscape.getVisualMappingManager();
-        if (!vmm.getCalculatorCatalog().getVisualStyleNames().contains("ThemeMapVS")) {
-            vmm.getCalculatorCatalog().addVisualStyle(new ThemeMapVisualStyle(vmm.getVisualStyle(),"ThemeMapVS"));
-        }
-		vmm.setVisualStyle("ThemeMapVS");
-		themaView.redrawGraph(false, false);
-		//HierarchicalLayoutListener hll = new HierarchicalLayoutListener();
-		AttributeCircleLayout all = new AttributeCircleLayout(thema,Cytoscape.getNodeAttributes(),themeAttributeName);
-		all.layout();
-		themaView.redrawGraph(true, false);
-		//hll will execute the layout algorithm on the currently selected view, which should be our newly created one
-		//hll.actionPerformed(null);
+		//iterate through the theme nodes, creating the THEME_MEMBER_COUNT attribute
+		nodeI = thema.nodesIterator();
+		while(nodeI.hasNext()) {
+			Node themeNode = nodeI.next();
+			int themeMemberCount = TMUtil.getNumThemeMembers(themeNode);
+			TMUtil.setNodeIntAttribute(themeNode,TM.theme_member_count_att_name,themeMemberCount);
+		}
+
 		
 		
 		//TODO consider creating 'relationship edges', ie edges between theme nodes which have member input nodes in common;  see: http://www.cgl.ucsf.edu/Research/cytoscape/groupAPI/doc/edu/ucsf/groups/GroupManager.html#regroupGroup(cytoscape.CyNode,%20boolean,%20boolean)
-		
+		return thema;
 	}
 	
 	/**Assigns the input graph node to the given theme node in the theme network, creating a new theme node in the theme network if one does 
@@ -141,7 +146,7 @@ public class ThematicMapFunctionPrototype {
 			int themeNodeI = themeNetwork.addNode(themeNodeRootI);
 			Node themeNode = themeNetwork.getNode(themeNodeI);
 			themeNode.setIdentifier(themeName);
-			TMUtil.setStringAtt(themeNode,themeNode.getIdentifier(),themeName);
+			//TMUtil.setStringAtt(themeNode,themeNode.getIdentifier(),themeName);
 			
 			themeNameToThemeNode.put(themeName, themeNode);
 		}
@@ -195,7 +200,11 @@ public class ThematicMapFunctionPrototype {
 					//create edge between theme nodes if it doesn't already exist
 					Node sourceThemeNode = Cytoscape.getCyNode(sourceNodeTheme);
 					Node targetThemeNode = Cytoscape.getCyNode(targetNodeTheme);
-					Edge themeEdge = Cytoscape.getCyEdge(sourceThemeNode,targetThemeNode,Semantics.INTERACTION,themeEdgeAliasVal,true,true);
+					
+					//TODO if we add a mode for capturing directedness, change directedness parameter in the following statements to 'true'
+					Edge themeEdge = Cytoscape.getCyEdge(sourceThemeNode,targetThemeNode,Semantics.INTERACTION,themeEdgeAliasVal,false,false);
+					//if the edge does not exist, create it.
+					if (themeEdge == null) themeEdge = Cytoscape.getCyEdge(sourceThemeNode,targetThemeNode,Semantics.INTERACTION,themeEdgeAliasVal,true,false);
 					
 					//recording the input edge's source and target nodes as an attribute of the theme edge
 					String themeEdgeAttributeVal = source.getIdentifier() + "-" + target.getIdentifier();
@@ -213,6 +222,25 @@ public class ThematicMapFunctionPrototype {
 				}
 			}
 		}
+		Set<String> ontologyNames = Cytoscape.getOntologyServer().getOntologyNames();
+		Cytoscape.getOntologyServer().getOntologies().get("");
+		cytoscape.data.ontology.Ontology ont = null;
+//		cytoscape.data.annotation.Ontology ont = null;
+		for (String ontologyName: Cytoscape.getOntologyServer().getOntologies().keySet()) {
+			System.out.println(ontologyName);
+			ont = Cytoscape.getOntologyServer().getOntologies().get(ontologyName);
+			System.out.println("Number of terms in ontology: " + ont.getTerms().size());
+//			Cytoscape.getBioDataServer().
+		}
+		
+		
+		Map mapSmallN = null;
+		Map mapSmallX = null;
+		int bigN = 0, bigX = 0;
+		
+//		DistributionCount distCount = new DistributionCount(null,null,null,null,null);
+		HypergeometricTestCalculate test = new HypergeometricTestCalculate(mapSmallN,mapSmallX,bigN,bigX);
+		test.calculate();
 	}
 
 	/**Modifies the passed map so that the Set which the Map maps the node's ID to contains the theme parameter.
@@ -233,4 +261,72 @@ public class ThematicMapFunctionPrototype {
 		inputNodeIDToThemes.put(id, themeSet);
 	}
 
+	
+	
+	public static CyNetworkView createThematicMapDefaultView(CyNetwork thema,String themeAttributeName) {
+		//finally, generate the graph view
+		CyNetworkView themaView = Cytoscape.createNetworkView(thema);
+		
+		
+		VisualMappingManager vmm = Cytoscape.getVisualMappingManager();
+		if (!vmm.getCalculatorCatalog().getVisualStyleNames().contains("ThemeMapVS")) {
+            vmm.getCalculatorCatalog().addVisualStyle(new ThemeMapVisualStyle(vmm.getVisualStyle(),"ThemeMapVS"));
+        }
+		vmm.setVisualStyle("ThemeMapVS");
+		themaView.redrawGraph(false, false);
+		//HierarchicalLayoutListener hll = new HierarchicalLayoutListener();
+		AttributeCircleLayout all = new AttributeCircleLayout(thema,Cytoscape.getNodeAttributes(),themeAttributeName);
+		all.layout();
+		themaView.redrawGraph(true, false);
+		//hll will execute the layout algorithm on the currently selected view, which should be our newly created one
+		//hll.actionPerformed(null);
+		
+		// prototype code for using a mapping to determine the size of the nodes, as opposed to hardcoding the nodesizecalculator, which is how it is currently done
+//		double defaultSize = 10.0;
+//		ContinuousMapping sizeMapping = new ContinuousMapping(defaultSize,ObjectMapping.NODE_MAPPING);
+//		sizeMapping.setControllingAttributeName(TM.theme_member_count_att_name, thema, true);
+//		VisualStyle themaVS = new ThemeMapVisualStyle(vmm.getVisualStyle(),"ThemeMapVS");
+		//themaVS.getNodeAppearanceCalculator().getNodeShapeCalculator().
+		
+		return themaView;
+	}
+
+	
+	private static CyNetwork ontology;
+	/**Sets the ontology network to use for the thematic map.  Nodes of the ontology are expected to be named after the 
+	 * theme names which they correspond to.
+	 * 
+	 * @param ontology the CyNetwork which represents the ontology of theme terms
+	 */
+	public static void setOntology(CyNetwork ontology) {
+		ThematicMapFunctionPrototype.ontology = ontology;
+	}
+	
+	/** Create a view for the ontology corresponding to the thematic map, using the theme term ontology visual style and appropriate context menu 
+	 * items.
+	 * 
+	 */
+	public static void createOntologyView() {
+		if (ontology == null) throw new RuntimeException("Cannot create view for Null Ontology");
+		registerThemeOntologyVisualStyle();
+		VisualMappingManager vmm = Cytoscape.getVisualMappingManager();
+		vmm.setVisualStyle(themeTermOntologyVSName);
+		Cytoscape.createNetworkView(ontology,ontology.getTitle() + " - Theme Term Ontology");
+		//TODO attach appropriate context menu listeners
+		
+	}
+	
+	private static final String themeTermOntologyVSName = "ThemeTermOntologyVS";
+	
+	/** Registers an instance of ThemeTermOntologyVisualStyle with the visual mapping manager's calculator catalog under the name "ThemeTermOntologyVS",
+	 *  for later use when creating theme term ontology network views.
+	 * 
+	 */
+	private static void registerThemeOntologyVisualStyle() {
+		VisualMappingManager vmm = Cytoscape.getVisualMappingManager();
+        if (!vmm.getCalculatorCatalog().getVisualStyleNames().contains(themeTermOntologyVSName)) {
+            vmm.getCalculatorCatalog().addVisualStyle(new ThemeTermOntologyVisualStyle(vmm.getVisualStyle(),themeTermOntologyVSName));
+        }
+	}
+	
 }
