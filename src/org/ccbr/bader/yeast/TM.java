2ee6201a32c62140bdb283b98fe9e809c070182a
Mike Matan <mmatan>
2007-06-10 16:03:26 +0000

    added new statif final field for defining the name of the theme member count attribute.

--- src/org/ccbr/bader/yeast/TM.java
+++ src/org/ccbr/bader/yeast/TM.java
@@ -4,5 +4,6 @@ public class TM {
 
 	public static final String memberListAttName = "THEME_MEMBERS";
 	public static final String edgeSourceAttName = "INPUT_EDGES";
+	public static final String theme_member_count_att_name = "THEME_MEMBER_COUNT";
 	
 }
